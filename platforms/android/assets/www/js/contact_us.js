a4aabdcb968c1ae7a39efd94bffc2497f4a465ba
juldio <garciajuliusb@yahoo.com>
2017-09-24 22:41:36 +0800

    contact us fixes

--- platforms/android/assets/www/js/contact_us.js
+++ platforms/android/assets/www/js/contact_us.js
@@ -13,6 +13,23 @@ $( document ).on( "pagebeforeshow", "#contactus-page", function() {
 
 $('#contactus-page').on('click','#btn-submit-contact',function(e) { 
 
+	if($('#txt_branch_name').val() == ''){
+		alert('Branch name is requried.');
+	}
+	else if($('#txt_fullname').val() == ''){
+		alert('Full name is required.');
+	}
+	else if($('#txt_email').val() == ''){
+		alert('Email address is required.');
+	}
+	else if($('#mobile_no').val() == ''){
+		alert('Mobile is required.');
+	}
+	else if($('#txt_message').val() == ''){
+		alert('Message is required.');
+	}
+	else{
+
         $.ajax({
             method: "POST",
             url: "http://app.banahawhealsspabranches.com/api/submitfeedback",
@@ -26,27 +43,15 @@ $('#contactus-page').on('click','#btn-submit-contact',function(e) {
             // location.reload();
         }).done(function( data, textStatus, jqXHR ) {
             console.log('LOGIN SUCCESS',data, textStatus, jqXHR);
-            alert(data);
-            // if (data.status=='success') {
-            //     $('#login-container').hide();
-            //     $('#welcome-message-container').show();
-            //     $('.display_name').html(data.profile.first_name);
-            //     //console.log(data);
-            //     //window.location = '{{url('plans')}}';
-                
-            //     @if(!in_array(Route::current()->getName(),['plans']))
-            //     //populateFavouritesDropDownList();
-            //     $('#loginModal').modal('hide') 
-            //     $('#addToFavouriteModal').modal({ keyboard: false,backdrop:'static' }) 
-            //     @else
-            //     window.location = '{{url('plans')}}';
-            //     @endif
-
-
-            // } else {
-            //      window.location.reload();
-            // }
+     
+            if (data.status=='success') {
+            	alert('Your message has been successfully sent.');
+                 window.location = 'contact_us.html';
+            } else {
+                 alert('Send Failed. Please try again.');
+            }
         });
+    }
 
 });
 
